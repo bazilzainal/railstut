1903b07e84f1a52c1f8280e55b2f363c1d0a260b
bazilzainal <bazil270@gmail.com>
2021-07-02 15:29:11 +0800

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def show
+    @user = User.find(params[:id])
+    @time = Time.now
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save # if this returns true, means that it is valid
+      flash[:success] = "Welcome to my app!"
+      redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
