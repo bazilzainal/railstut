81468603704c7a46e25714a5a952a930d69210be
bazilzainal <bazil270@gmail.com>
2021-08-09 14:14:43 +0800

    Add flash when logging out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,7 @@ class SessionsController < ApplicationController
   def destroy
     log_out
     redirect_to root_url
+    flash[:success] = "Logged out!"
   end
 
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,7 +7,7 @@ module SessionsHelper
 
     def log_out
         session.delete(:user_id)
-        @current_user = nil?
+        @current_user = nil
     end
 
     # Returns the current logged-in user (if any).
